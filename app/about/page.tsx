e7ab8cb42c51c4fabc24a41da1f34235c3580ba9
AsadMughalAM <am127955@gmail.com>
2026-01-12 16:59:03 +0500

    Refactor About page for improved code formatting and consistency

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,12 @@
-"use client"
-import React from 'react'
-import AboutSection from '@/components/sections/About'
-import Services from '@/components/sections/Services'
-import SlidingText from '@/components/sections/SlidingText'
-import Team from '@/components/sections/Team'
-import Counter from '@/components/sections/Counter'
-import Testimonial from '@/components/sections/Testimonial'
-import Blog from '@/components/sections/Blog'
+"use client";
+import React from "react";
+import AboutSection from "@/components/sections/About";
+import Services from "@/components/sections/Services";
+import SlidingText from "@/components/sections/SlidingText";
+import Team from "@/components/sections/Team";
+import Counter from "@/components/sections/Counter";
+import Testimonial from "@/components/sections/Testimonial";
+import Blog from "@/components/sections/Blog";
 
 const About = () => {
   return (
@@ -14,15 +14,22 @@ const About = () => {
       <section className="page-header">
         <div
           className="page-header__bg"
-          style={{ backgroundImage: 'url(/assets/images/backgrounds/page-header-bg.jpg)' }}
+          style={{
+            backgroundImage:
+              "url(/assets/images/backgrounds/page-header-bg.jpg)",
+          }}
         ></div>
         <div className="container">
           <div className="page-header__inner">
             <h3>About Us</h3>
             <div className="thm-breadcrumb__inner">
               <ul className="thm-breadcrumb list-unstyled">
-                <li><a href="/">Home</a></li>
-                <li><span className="icon-arrow-angle-pointing-to-right"></span></li>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <span className="icon-arrow-angle-pointing-to-right"></span>
+                </li>
                 <li>About Us</li>
               </ul>
             </div>
@@ -37,9 +44,8 @@ const About = () => {
       <Counter />
       <Testimonial />
       <Blog />
-     
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
