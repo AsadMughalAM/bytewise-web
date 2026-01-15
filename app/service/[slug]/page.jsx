7f66fbc4c02aac1754568413730e89801e58a817
AsadMughalAM <am127955@gmail.com>
2026-01-15 13:34:10 +0500

    feat: create dynamic service page to display slug parameter.

--- app/service/[slug]/page.jsx
+++ app/service/[slug]/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const page = () => {
-  return <div>page 1</div>;
+const page = async ({ params }) => {
+  const { slug } = await params;
+  return <div>page 1 {slug}</div>;
 };
 
 export default page;
