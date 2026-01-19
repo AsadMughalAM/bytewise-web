8d99ef4b34123751d957746d410ee77f4499f1b7
AsadMughalAM <am127955@gmail.com>
2026-01-19 14:40:14 +0500

    feat: add SlidingText component to display service titles in a marquee.

--- components/sections/SlidingText.jsx
+++ components/sections/SlidingText.jsx
@@ -1,12 +1,34 @@
+"use client";
+import { useEffect, useRef } from "react";
 import { services } from "@/data/servicesData";
 
 const SlidingText = () => {
   const items = services.map((s) => s.title);
+  const marqueeRef = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.jQuery) {
+      const $ = window.jQuery;
+
+      // Initialize marquee
+      if ($(marqueeRef.current).length) {
+        $(marqueeRef.current).marquee({
+          speed: 40,
+          gap: 20,
+          delayBeforeStart: 0,
+          direction: "left",
+          duplicated: true,
+          pauseOnHover: true,
+          startVisible: true,
+        });
+      }
+    }
+  }, []);
 
   return (
     <section className="sliding-text-one">
       <div className="sliding-text-one__wrap">
-        <ul className="sliding-text-one__list list-unstyled marquee_mode-1">
+        <ul ref={marqueeRef} className="sliding-text-one__list list-unstyled">
           {items.map((item, index) => (
             <li key={index}>
               <h2 data-hover={item} className="sliding-text-one__title">
